9d4eb4a38a1978dedd89af083b018d5064d0fdf8
Владислав Пустовит <v.v.pustovit2000@gmail.com>
2018-12-09 10:51:26 +0200

    initial commit

---
+++ LR_05/LR_05/LR_05.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "windows.h"
+#include "math.h"
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	double Y;
+	int x;
+	cout << "Введіть х\nx=";
+	cin >> x;
+	int sum = 0;
+	for (int i = 1; i < 3; i++)
+	{
+		sum = sum + i * x;
+	}
+	Y = pow(x, 3) + sin(sum);
+	cout << "\nY=" << Y << endl;
+	system("pause");
+	return 0;
+}
